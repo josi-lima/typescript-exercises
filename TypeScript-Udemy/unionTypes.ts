c52bc744cf103513fadd57fb25c8fda62b46181f
Josiane de Lima <teacher.josi@yahoo.com>
2023-09-18 14:37:18 -0300

    :books: docs: add examples of union types

--- TypeScript-Udemy/unionTypes.ts
+++ TypeScript-Udemy/unionTypes.ts
@@ -1,9 +1,15 @@
 // *************** UNION TYPES *******************
 
+// union types allow us to give a value a few different possible types by using the pipe character
+
 // Basic Union Type:
 let age: number | string = 21;
+
+// reasigning the variable
 age = 23;
 age = "24";
+
+// ==========================================
 
 type Point = {
   x: number;
@@ -15,45 +21,56 @@ type Loc = {
   long: number;
 };
 
-// Union type with type aliases
-let position: Point | Loc = { x: 1, y: 34 };
+// Union type with type aliases, use let to be able to alter it
+let position: Point | Loc = { x: 120, y: 34 };
 position = { lat: 321.213, long: 23.334 };
 
-// Function parameter union type:
+// Union Type with Arrays
+const coords: (Point | Loc)[] = [];
+coords.push({ lat: 321.213, long: 23.334 });
+coords.push({ x: 213, y: 43 });
+
+// ==========================================
+
+// Function with a parameter union type that accepts a type number or string
 function printAge(age: number | string): void {
-  console.log(`You are ${age} years old`);
+  console.log(`You are ${age} years old.`);
 }
+printAge(38);
+
+// -----------------------------------------------
+
+// On this example, price could be a number or a string depending on the $ sign in it
+// the conditional establishes a type narrowing
 
 function calculateTax(price: number | string, tax: number) {
   if (typeof price === 'string') {
     price = parseFloat(price.replace("$", ""));
   }
-  return price * tax;
+  return (price * tax).toFixed(2);
 }
+console.log(calculateTax('$130.50', .07))   // 9.14
 
-// const nums: number[] = [1,2,3,4]
-// const stuff: any[] = [1,2,3,4, true, "asdas", {}]
+// ==========================================
 
-// const stuff: (number | string)[] = [1,2,3, "das"]
-// const stuff: number[] | string[] = ["asd", "asd", 1]
+// Examples of regular array types
+const countdown: number[] = [5, 4, 3, 2, 1];
+const stuff: any[] = [1,2,3,4, true, "cake", {}];
 
-// Union Type With Arrays
-const coords: (Point | Loc)[] = [];
-coords.push({ lat: 321.213, long: 23.334 });
-coords.push({ x: 213, y: 43 });
+// union type of numbers OR strings as a single array
+const basket: (number | string)[] = [10, 78, 30, 'luckyNumbers'];
 
-// Literal Types
-let zero: 0 = 0;
-let mood: "Happy" | "Sad" = "Happy";
-mood = "Sad";
+// -----------------------------------------------
 
-type DayOfWeek =
-  | "Monday"
-  | "Tuesday"
-  | "Wednesday"
-  | "Thursday"
-  | "Friday"
-  | "Saturday"
-  | "Sunday";
+// Don't confuse with this other case
 
-let today: DayOfWeek = "Sunday";
+// union type of an array of numbers OR an array of strings 
+let differentItems: number[] | string[] = [89, 25, 33, 4];
+differentItems = ['ruby', 'esmerald', 'diamond'];
+
+console.log(differentItems);
+// [ 'ruby', 'esmerald', 'diamond' ]
+
+// ==========================================
+
+
