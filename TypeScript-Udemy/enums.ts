ac970651fa8c8dce435916c77130444264ba3628
Josiane de Lima <teacher.josi@yahoo.com>
2023-09-19 16:08:38 -0300

    :books: feat: update examples of enums

--- TypeScript-Udemy/enums.ts
+++ TypeScript-Udemy/enums.ts
@@ -3,17 +3,22 @@
 // enums allow us to define a set of named constants we can reuse throughout our code.
 // we can give these constants numeric or string values
 
-// numeric enums
+
+// hover the enums to check their values
 enum repliesA {
     oui,
     non,
     certainement,
 }
 
+// when we create an enum and we don't specify any values, TypeScript just assigns an implicit numeric value starting at zero upwards.
+
+// if we assign a numeric value to an enum, the implicit one is replaced
+
 enum repliesB {
-    oui = 1,
-    non = 2,
-    certainement = 3,
+    oui = 10,
+    non = 20,
+    certainement = 30,
 }
 
 // ==========================================
@@ -26,10 +31,19 @@ enum grades {
     d = 'Failed',
 }
 
+// --------------------------------
+
+enum ArrowKeys {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right",
+  }
+  
 // ==========================================
 
 // heterogenous enums
-enum grades {
+enum direction {
     north = 'North',
     south = 1,
     east = 2,
@@ -53,14 +67,17 @@ const isDelivered = (status: OrderStatus) => {
 }
 console.log(isDelivered(OrderStatus.RETURNED));   // false
 
-// ==========================================
+// -----------------------------------
 
-// String Enum:
-enum ArrowKeys {
-  UP = "up",
-  DOWN = "down",
-  LEFT = "left",
-  RIGHT = "right",
+// also possible:
+
+const enum MyOrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED,
 }
+
+// assigning enums to a variable avoids all the extra code enums generate when code in TypeScript is compiled to JavaScript
 
 // ==========================================
