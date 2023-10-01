cb855634d64e4779b77bc2df253ff193ab756542
Josiane de Lima <teacher.josi@yahoo.com>
2023-10-01 18:50:34 -0300

    :books: feat: add type narrowing examples

---
+++ TypeScript-Udemy/narrowing.ts
@@ -0,0 +1,176 @@
+// *************** TYPE NARROWING *******************
+
+// typeof Type Guards involves simply doing a type check before working with a value
+
+// typeof works with primitive types
+
+function triple(value: number | string) {
+  if (typeof value === "string") {
+    return value.repeat(3);
+  }
+  return value * 3;
+}
+console.log(triple("ha"));
+// hahaha
+
+console.log(triple(25));  // 75
+
+// =============================================
+
+// truthiness type guards involve checking a value for being truthy or falsy before working with it 
+
+// truthiness narrowing
+const printLetters = (word?: string): string | undefined => {
+  if (word) {
+    for (let char of word) {
+      return char;
+    }
+  } else {
+    return "INVISIBLE WORD!";
+  }
+};
+
+console.log(printLetters());
+// INVISIBLE WORD!
+
+// =============================================
+
+// equality narrowing
+function someDemo(x: string | number, y: string | boolean) {
+  if (x === y) {
+    x.toUpperCase();
+  }
+}
+
+// IN Operator Narrowing
+interface Movies {
+  title: string;
+  duration: number;
+}
+
+interface TVShow {
+  title: string;
+  numEpisodes: number;
+  episodeDuration: number;
+}
+
+function getRuntime(media: Movies | TVShow) {
+  if ("numEpisodes" in media) {
+    return media.numEpisodes * media.episodeDuration;
+  }
+  return media.duration;
+}
+
+console.log(getRuntime({ title: "Amadeus", duration: 140 }));
+console.log(
+  getRuntime({ title: "Spongebob", numEpisodes: 80, episodeDuration: 30 })
+);
+
+// Instanceof Narrowing:
+function printFullDate(date: string | Date) {
+  if (date instanceof Date) {
+    console.log(date.toUTCString());
+  } else {
+    console.log(new Date(date).toUTCString());
+  }
+}
+
+// Instanceof Narrowing:
+class User {
+  constructor(public username: string) {}
+}
+class Company {
+  constructor(public name: string) {}
+}
+
+function printName(entity: User | Company) {
+  if (entity instanceof User) {
+    entity;
+  } else {
+    entity;
+  }
+}
+
+// Type Predicates
+
+interface Cat {
+  name: string;
+  numLives: number;
+}
+interface Dog {
+  name: string;
+  breed: string;
+}
+
+function isCat(animal: Cat | Dog): animal is Cat {
+  return (animal as Cat).numLives !== undefined;
+}
+
+function makeNoise(animal: Cat | Dog): string {
+  if (isCat(animal)) {
+    animal;
+    return "Meow";
+  } else {
+    animal;
+    return "Woof!";
+  }
+}
+
+// Discriminated Unions
+interface Rooster {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "rooster";
+}
+
+interface Cow {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "cow";
+}
+
+interface Pig {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "pig";
+}
+
+interface Sheep {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "sheep";
+}
+
+type FarmAnimal = Pig | Rooster | Cow | Sheep;
+
+function getFarmAnimalSound(animal: FarmAnimal) {
+  switch (animal.kind) {
+    case "pig":
+      return "Oink!";
+    case "cow":
+      return "Moooo!";
+    case "rooster":
+      return "Cockadoodledoo!";
+    case "sheep":
+      return "Baaa!";
+    default:
+      // We should never make it here, if we handled all cases correctly
+      //   const shouldNeverGetHere: never = animal;
+      //   return shouldNeverGetHere
+      const _exhaustiveCheck: never = animal;
+      return _exhaustiveCheck;
+  }
+}
+
+const stevie: Rooster = {
+  name: "Stevie Chicks",
+  weight: 2,
+  age: 1.5,
+  kind: "rooster",
+};
+
+console.log(getFarmAnimalSound(stevie));
